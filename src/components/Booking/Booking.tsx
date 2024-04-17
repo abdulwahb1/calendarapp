e9167b7b66a6d4eb7b7ec705f03cb96467e6dcc2
Abdul Wahab <abdulwabby@gmail.com>
2024-04-17 12:08:35 +0500

    connected_db

--- src/components/Booking/Booking.tsx
+++ src/components/Booking/Booking.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
-import Form from "../Form/Form";
 import {
   Dialog,
   DialogContent,
@@ -14,14 +13,61 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { supabase } from "../../utils/supabase";
+import { useEffect } from "react";
 
 type ValuePiece = Date | null;
 
 type Value = ValuePiece | [ValuePiece, ValuePiece];
-function Booking() {
+
+export default function Booking() {
   const [value, onChange] = useState<Value>(new Date());
   const [selectedDate, setSelectedDate] = useState<Date | null>(null);
   const [showModal, setShowModal] = useState(false);
+  const [isEvent, setEvent] = useState<string>("");
+  const [isRem, setRem] = useState<string>("");
+  const [isEventName, setEventName] = useState([""]);
+
+  const fetchEvent = async () => {
+    const { data, error } = await supabase
+      .from("calendar")
+      .select("event_name");
+
+    if (error) {
+      alert(JSON.stringify(error));
+    } else {
+      const eventdata = data.map((item) => item.event_name);
+      setEventName(eventdata);
+    }
+  };
+
+  useEffect(() => {
+    fetchEvent();
+  }, []);
+
+  console.log(isEventName);
+
+  const handleSubmission = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const { data, error } = await supabase
+      .from("calendar")
+      .insert({ event_name: isEvent, reminder: isRem, date: selectedDate })
+      .select();
+
+    if (error) {
+      alert(JSON.stringify(error));
+    } else {
+      setEvent("");
+    }
+  };
 
   const handleDateChange = (value: Value) => {
     onChange(value);
@@ -47,6 +93,7 @@ function Booking() {
         value={value}
         onChange={handleDateChange}
       />
+      <h2>Selected Date: {selectedDate?.toString()}</h2>
       {showModal && (
         <div className="modal">
           <Dialog>
@@ -60,10 +107,41 @@ function Booking() {
                   Add events to your calender here.
                 </DialogDescription>
               </DialogHeader>
-              <Form />
-              <DialogFooter>
-                <Button onClick={closeModal}>Close</Button>
-              </DialogFooter>
+              <form onSubmit={handleSubmission}>
+                <div className="grid gap-4 py-4">
+                  <div className="grid grid-cols-4 items-center gap-4">
+                    <Label htmlFor="name" className="text-right">
+                      Event
+                    </Label>
+                    <Input
+                      id="event"
+                      placeholder="Event name"
+                      className="col-span-3"
+                      value={isEvent}
+                      onChange={(e) => setEvent(e.target.value)}
+                    />
+                  </div>
+                  <div className="grid grid-cols-4 items-center gap-4">
+                    <Label htmlFor="username" className="text-right">
+                      Reminder
+                    </Label>
+                    <Select value={isRem} onValueChange={setRem}>
+                      <SelectTrigger className="w-[180px]">
+                        <SelectValue placeholder="Choose time" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectItem value="1hr">1 hr</SelectItem>
+                        <SelectItem value="2hr">2 hr</SelectItem>
+                        <SelectItem value="12hr">12 hr</SelectItem>
+                      </SelectContent>
+                    </Select>
+                  </div>
+                </div>
+                <DialogFooter>
+                  <Button type="submit">Save</Button>
+                  <Button onClick={closeModal}>Close</Button>
+                </DialogFooter>
+              </form>
             </DialogContent>
           </Dialog>
         </div>
@@ -71,5 +149,3 @@ function Booking() {
     </div>
   );
 }
-
-export default Booking;
--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -10,7 +10,15 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { Button } from "@/components/ui/button";
-import { DialogFooter } from "@/components/ui/dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 const Form = () => {
   const [isEvent, setEvent] = useState<string[]>([]);
@@ -20,35 +28,56 @@ const Form = () => {
     setEvent([...isEvent]);
   };
 
+  const closeModal = () => {
+    false;
+  };
+
   return (
-    <form>
-      <div className="grid gap-4 py-4">
-        <div className="grid grid-cols-4 items-center gap-4">
-          <Label htmlFor="name" className="text-right">
-            Event
-          </Label>
-          <Input id="event" placeholder="Event name" className="col-span-3" />
-        </div>
-        <div className="grid grid-cols-4 items-center gap-4">
-          <Label htmlFor="username" className="text-right">
-            Reminder
-          </Label>
-          <Select>
-            <SelectTrigger className="w-[180px]">
-              <SelectValue placeholder="1 hr" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value="1hr">1 hr</SelectItem>
-              <SelectItem value="2hr">2 hr</SelectItem>
-              <SelectItem value="12hr">12 hr</SelectItem>
-            </SelectContent>
-          </Select>
-        </div>
-      </div>
-      <DialogFooter>
-        <Button type="submit">Save</Button>
-      </DialogFooter>
-    </form>
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline">Add Event</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Add Event</DialogTitle>
+          <DialogDescription>
+            Add events to your calender here.
+          </DialogDescription>
+        </DialogHeader>
+        <form>
+          <div className="grid gap-4 py-4">
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="name" className="text-right">
+                Event
+              </Label>
+              <Input
+                id="event"
+                placeholder="Event name"
+                className="col-span-3"
+              />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="username" className="text-right">
+                Reminder
+              </Label>
+              <Select>
+                <SelectTrigger className="w-[180px]">
+                  <SelectValue placeholder="1 hr" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="1hr">1 hr</SelectItem>
+                  <SelectItem value="2hr">2 hr</SelectItem>
+                  <SelectItem value="12hr">12 hr</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+          </div>
+          <DialogFooter>
+            <Button type="submit">Save</Button>
+          </DialogFooter>
+        </form>
+      </DialogContent>
+    </Dialog>
   );
 };
 
