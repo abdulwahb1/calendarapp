bc749d7df4853c6287420309f28195bed7a88e95
Abdul Wahab <abdulwabby@gmail.com>
2024-05-11 22:35:06 +0500

    Refactored

---
+++ src/pages/BookingPage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Booking from "@/components/Booking/Booking";
+
+const BookingPage = () => {
+  return (
+    <section className="flex flex-col items-center">
+      <div>
+        <Booking />
+      </div>
+    </section>
+  );
+};
+
+export default BookingPage;
