f1e255b60f7df24462b714e1508a4cc5992ebd17
Abdul Wahab <abdulwabby@gmail.com>
2024-05-15 18:49:00 +0500

    ui_overhaul

--- src/pages/BookingPage.tsx
+++ src/pages/BookingPage.tsx
@@ -3,8 +3,9 @@ import Booking from "@/components/Booking/Booking";
 
 const BookingPage = () => {
   return (
-    <section className="flex flex-col items-center">
-      <div>
+    <section className="flex">
+      <div className=" w-2/12 bg-[#d7d8f7] min-h-screen"></div>
+      <div className="flex flex-col justify-center items-center mx-auto">
         <Booking />
       </div>
     </section>
